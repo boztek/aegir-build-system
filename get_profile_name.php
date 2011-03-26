63228b4815315d21abfe7391c15fee5c3ca15e60
Boris Gordon <boris@boztek.net>
2011-03-26 18:27:19 +1100

    Use aegir user's copy of drush and drush make

--- get_profile_name.php
+++ get_profile_name.php
@@ -4,11 +4,11 @@
  * Print out profile repo parsed from a drush make build stub
  */
 
-define('DRUSH_BASE_PATH', '/usr/local/drush');
-define('DRUSH_MAKE_PATH', '/Users/boris/.drush/drush_make');
+define('DRUSH_BASE_PATH', '/var/aegir/drush');
+define('DRUSH_MAKE_PATH', '/var/aegir/.drush/drush_make');
 require_once DRUSH_BASE_PATH . '/includes/drush.inc';
 require_once DRUSH_BASE_PATH . '/includes/context.inc';
-require_once DRUSH_BASE_PATH . '/includes/exec.inc';
+// require_once DRUSH_BASE_PATH . '/includes/exec.inc';
 require_once DRUSH_MAKE_PATH . '/drush_make.drush.inc';
 
 $info = drush_make_parse_info_file($argv[1]);
