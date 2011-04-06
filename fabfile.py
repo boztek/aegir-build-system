1467eb697dd52447e48dc58b314935b0fdec95b0
Boris Gordon <boris@boztek.net>
2011-04-06 12:38:02 +1000

    split out platform build code and create release from stub functionality

--- fabfile.py
+++ fabfile.py
@@ -2,8 +2,13 @@ from fabric.api import local, settings, abort, run, cd, lcd, env
 from fabric.contrib.console import confirm
 from datetime import date
 
-# Will migrate site_uri if it exists otherwise provision a new site
+def release(stub, tag, site_uri, sync_uri):
+    """Build a platform from a tag and sync db and files from another site"""
+    build(stub, release=tag)
+    sync_site(sync_uri, site_uri)
+
 def provision_site(site_uri, platform_id, app_id):
+    """If site_uri exists migrate to platform_id else install new site"""
     with settings(warn_only=True):
         existing_site = local('drush sa |grep "%s"' % site_uri, True)
     if existing_site:
@@ -24,10 +29,28 @@ def provision_site(site_uri, platform_id, app_id):
         local('php /var/aegir/drush/drush.php provision-install @%s' % 
             (site_uri))
 
-def build(stub, branch='develop', site_uri=None, migrate=True):
+def build_platform(buildfile, platform_id):
+    """Build a platform from a build level drush make file using provision"""
+    # exit if platform exists
+    with settings(warn_only=True):
+        existing_platform = local('drush sa |grep "platform_%s"' % platform_id, True)
+        if (existing_platform):
+            exit("PLATFORM EXISTS")
+        else
+            print "Platform to be built: @platform_" + platform_id
+    path_to_platform = "/var/aegir/platforms/" + app_id + "/" + platform_id
+    local('drush provision-save @platform_%s --context_type=platform \
+            --root="%s" --makefile="%s"' % 
+            (platform_id, path_to_platform, buildfile))
+    local('drush provision-verify @platform_%s' % platform_id)
+    local('php /var/aegir/drush/drush.php @hostmaster hosting-dispatch')
+    local('drush @hostmaster hosting-import @platform_%s' % platform_id)
+
+def build(stub, branch=None, site_uri=None, migrate=True, release=None):
     repo = local('php get_profile_repo.php %s' % stub, True)
     app_id = local('php get_profile_name.php %s' % stub, True)
-
+    if (not branch):
+        branch = local('php get_profile_branch.php %s' % stub, True)
     # build id is now project-SHA1 where SHA1 is head of branch
     tmp_repo = '/tmp/' + app_id
     local('rm -rf %s' % (tmp_repo))
@@ -35,27 +58,9 @@ def build(stub, branch='develop', site_uri=None, migrate=True):
     with lcd(tmp_repo):
         commit_id = local('git log --format="%h" -1', True)
     local('rm -rf %s' % (tmp_repo))
-
     # if build of this commit already exists quit
     platform_id = app_id + commit_id
-    with settings(warn_only=True):
-        existing_platform = local('drush sa |grep "platform_%s"' % platform_id, True)
-        if (existing_platform):
-            exit("PLATFORM EXISTS")
-    print "Platform to be built: @platform_" + platform_id
-
-    path_to_platform = "/var/aegir/platforms/" + app_id + "/" + platform_id
-    makefile = stub
-    local('drush provision-save @platform_%s --context_type=platform \
-                --root="%s" --makefile="%s"' % (platform_id, path_to_platform, makefile))
-    local('drush provision-verify @platform_%s' % platform_id)
-    local('php /var/aegir/drush/drush.php @hostmaster hosting-dispatch')
-    local('drush @hostmaster hosting-import @platform_%s' % platform_id)
-
+    build_platform(stub, platform_id)
     # migrate site
-    if (site_uri):
-        provision_site(site_uri, platform_id, app_id) unless (migrate == False)
-    
-
-    # clean up old platform(s)
-    #
+    if (site_uri and migrate):
+        provision_site(site_uri, platform_id, app_id)
