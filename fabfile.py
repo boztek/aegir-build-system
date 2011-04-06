6b9f82e977028a10943fcb7e95056f4f2e14a0f3
Boris Gordon <boris@boztek.net>
2011-04-07 01:28:48 +1000

    site provisioning still needs work

--- fabfile.py
+++ fabfile.py
@@ -21,6 +21,7 @@ def release(repo, tag, site_uri, sync_uri=None):
     if (sync_uri):
         sync_site(sync_uri, site_uri)
 
+
 def sync_site(source_site, dest_site):
     """Delete dest_site instance and clone from source_site with provision"""
     platform_id = __get_alias_variable(dest_site, 'platform')
@@ -38,6 +39,7 @@ def sync_site(source_site, dest_site):
     local('php /var/aegir/drush/drush.php @hostmaster hosting-task \
         @%s verify'% (dest_site))
 
+
 def delete_site(site_uri):
     """Disable and delete a site instance after making a backup"""
     local('php /var/aegir/drush/drush.php @%s provision-backup' % site_uri)
@@ -48,6 +50,7 @@ def delete_site(site_uri):
         hosting-task @%s delete' % site_uri)
     local('php /var/aegir/drush/drush.php @hostmaster hosting-dispatch')
 
+
 def provision_site(site_uri, platform_id, app_id):
     """If site_uri exists migrate to platform_id else install new site"""
     with settings(warn_only=True):
@@ -56,8 +59,9 @@ def provision_site(site_uri, platform_id, app_id):
         local('php /var/aegir/drush/drush.php @hostmaster hosting-task @platform_%s verify' % platform_id)
         local('php /var/aegir/drush/drush.php @hostmaster hosting-dispatch')
         local("php /var/aegir/drush/drush.php @%s provision-migrate '@platform_%s'" % (site_uri, platform_id))
-        local("php /var/aegir/drush/drush.php --uri='%s' --platform='@platform_%s' --root='/var/aegir/platforms/%s/%s' --profile='%s' provision-save '@%s'" % (site_uri, platform_id, app_id, platform_id, app_id, site_uri))
-        backup = local('ls -t /var/aegir/backups/%s-* | head -1' % (app_id), True)
+        # local("php /var/aegir/drush/drush.php --uri='%s' --platform='@platform_%s' --root='/var/aegir/platforms/%s/%s' --profile='%s' provision-save '@%s'" % (site_uri, platform_id, app_id, platform_id, app_id, site_uri))
+        backup = local('ls -t /var/aegir/backups/%s-* | head -1' % (site_uri), True)
+        print "Deploying from backup: " + backup
         local("php /var/aegir/drush/drush.php --old_uri='%s' @%s provision-deploy %s" % (site_uri, site_uri, backup))
         local("php /var/aegir/drush/drush.php @hostmaster hosting-import @%s" % (site_uri))
         local("php /var/aegir/drush/drush.php @hostmaster hosting-task @platform_%s verify" % platform_id)
@@ -70,6 +74,7 @@ def provision_site(site_uri, platform_id, app_id):
         #     (site_uri, platform_id, site_uri))
         # local('php /var/aegir/drush/drush.php provision-install @%s' % 
         #     (site_uri))
+
 
 def build_platform(buildfile, platform_id, app_id, server):
     """Build a platform from a build level drush make file using provision"""
@@ -87,6 +92,7 @@ def build_platform(buildfile, platform_id, app_id, server):
     local('php /var/aegir/drush/drush.php provision-verify @platform_%s' % platform_id)
     local('php /var/aegir/drush/drush.php @hostmaster hosting-dispatch')
     local('php /var/aegir/drush/drush.php @hostmaster hosting-import @platform_%s' % platform_id)
+
 
 def build(git_url, branch='develop', site_uri=None, server=None):
     """Check out source code and extract platform build stub from repo and build platform with provision"""
